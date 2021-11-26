82fffbb5c7ab1c62610b8e54c09d3e2c85091b5e
0639Quercus <thsomsits.brie@gmail.com>
2021-11-26 16:07:06 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -2,8 +2,29 @@
 using namespace std;
 
 int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
+
+    int num[128];
+    int input, len = 0, odds = 0, evens = 0;
+    while (true) {
+        cout << "Enter an integer: ";
+        cin >> input;
+        if (input == 0) {
+            break;
+        }
+        num[len] = input;
+        len++;
+    }
+
+    for (int i = 0; i < len; i++) {
+        if (num[i]%2 == 0) {
+            evens += 1;
+        }
+        else {
+            odds += 1;
+        }
+    }
+
+    cout << "#Even numbers = " << evens << endl;
+    cout << "#Odd numbers = " << odds << endl;
     return 0;
 }
